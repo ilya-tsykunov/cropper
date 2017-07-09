4066904735fc325ba3339ba3cc73c679fe1bb42a
Ilya Tsykunov <ilya.tcykunov@gmail.com>
2017-07-09 23:58:33 +0300

    Sidekiq config chacnged

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 22 }
+    config.redis = { url: ENV['REDIS_URL'], size: 5 }
 
     Rails.application.config.after_initialize do
       Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
