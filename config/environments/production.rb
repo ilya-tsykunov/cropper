074b61a16e51479d9b1cd7115dc2cd75df7f1da2
Ilya Tsykunov <ilya.tcykunov@gmail.com>
2017-07-10 21:23:14 +0300

    Mail settings for production env are changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,7 @@
+require 'rubygems' if RUBY_VERSION < '1.9'
+require 'rest_client'
+require 'json'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -65,6 +69,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}"
+  first_inbox = JSON.parse(response)[0] # get first inbox
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: first_inbox['username'],
+    password: first_inbox['password'],
+    address: first_inbox['domain'],
+    domain: first_inbox['domain'],
+    port: first_inbox['smtp_ports'][0],
+    authentication: :plain
+  }
+  config.action_mailer.default_url_options = { host: ENV['APP_NAME'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
