131f9ac01b1d6e86c5ec9670a9947db991dd88b3
Ilya Tsykunov <ilya.tcykunov@gmail.com>
2017-07-10 19:40:24 +0300

    Sidekiq config fixed

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,24 @@
 if Rails.env.production?
+  redis_url = ENV['REDISCLOUD_URL'].presence || ENV['REDIS_URL'].presence || 'redis://localhost:6379/0'
+
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 2 }
+    config.redis = { url: redis_url, size: 2 }
   end
 
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 5 }
+  # https://github.com/mperham/sidekiq/wiki/Using-Redis#complete-control
+  #
+  number_of_sidekiq_connections =
+    if ENV['RAILS_MAX_THREADS'].present?
+      ENV['RAILS_MAX_THREADS'] + 2
+    else
+      5
+    end
 
+  Sidekiq.configure_server do |config|
+    config.redis = { url: redis_url, size: number_of_sidekiq_connections }
+
+    # http://julianee.com/rails-sidekiq-and-heroku/
+    #
     Rails.application.config.after_initialize do
       Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
       ActiveRecord::Base.connection_pool.disconnect!
