111cebb3324141d660ab6f612dd5881ac20e43ee
Ilya Tsykunov <ilya.tcykunov@gmail.com>
2017-07-10 20:08:46 +0300

    Event notifications enabled

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,7 @@ class Event < ApplicationRecord
   validates :name, :city, :start_date, :end_date, :topics, presence: true
   validate :end_date_more_than_start_date
 
-  # Disabled until production server is configured properly
-  #
-  # after_create :schedule_event_notification
+  after_create :schedule_event_notification
 
   private
 
